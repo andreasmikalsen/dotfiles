3f12040521782338c44583726dabad4a68a8f7dd
Andreas Mikalsen <32385759+andreasmikalsen@users.noreply.github.com>
2026-08-23 14:32:32 +0200

    feat: wezterm (#5)
    * feat: wezterm
    
    * feat: wezterm

--- setup.ps1
+++ setup.ps1
@@ -157,7 +157,8 @@ Ensure-WingetPackage "Neovim.Neovim" "Neovim"
 Ensure-WingetPackage "Nushell.Nushell" "Nushell" @("--scope", "machine")
 Ensure-WingetPackage "Starship.Starship" "Starship"
 Ensure-WingetPackage "GitHub.cli" "GitHub CLI"
-Ensure-WingetPackage "dandavison.delta" "delta"
+Ensure-WingetPackage "dandavison.delta" "Delta"
+Ensure-WingetPackage "wez.wezterm" "Wezterm"
 
 # npm
 Ensure-NpmGlobal "tree-sitter-cli"
@@ -174,19 +175,22 @@ Write-Host "=== Finished installing dependencies ==="
 # Setup junctions
 $repoRoot = Split-Path -Parent $MyInvocation.MyCommand.Path
 
-$nvimSource   = Join-Path $repoRoot "nvim"
-$nuSource     = Join-Path $repoRoot "nushell"
-$ghDashSource = Join-Path $repoRoot "gh-dash"
+$nvimSource    = Join-Path $repoRoot "nvim"
+$nuSource      = Join-Path $repoRoot "nushell"
+$ghDashSource  = Join-Path $repoRoot "gh-dash"
+$weztermSource = Join-Path $repoRoot "wezterm"
 
-$nvimTarget   = Join-Path $env:LOCALAPPDATA "nvim"
-$nuTarget     = Join-Path $env:APPDATA "nushell"
-$ghDashTarget = "~/.config/gh-dash"
+$nvimTarget    = Join-Path $env:LOCALAPPDATA "nvim"
+$nuTarget      = Join-Path $env:APPDATA "nushell"
+$ghDashTarget  = "~/.config/gh-dash"
+$weztermTarget = Join-Path $env:USERPROFILE ".config\wezterm"
 
 Write-Host "=== Setup junctions ==="
 
 Create-Junction $nvimTarget $nvimSource
 Create-Junction $nuTarget $nuSource
 Create-Junction $ghDashTarget $ghDashSource
+Create-Junction $weztermTarget $weztermSource
 
 Write-Host ""
 Write-Host "=== Finished setting up junctions ==="
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,294 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+local act = wezterm.action
+
+local function ActivateThing(direction)
+  local function callback(window, pane)
+    local tab = window:active_tab()
+    local panes = tab:panes_with_info()
+
+    local index = 1
+    local is_zoomed = false
+    for i, p in ipairs(panes) do
+      if p.is_active then
+        index = i
+        is_zoomed = p.is_zoomed
+        break
+      end
+    end
+
+    if direction == "next" then
+      if index < #panes then
+        panes[index+1].pane:activate()
+      else
+        window:perform_action(act.ActivateTabRelative(1), pane)
+      end
+    elseif direction == "prev" then
+      if index > 1 then
+        panes[index-1].pane:activate()
+      else
+        window:perform_action(act.ActivateTabRelative(-1), pane)
+      end
+    end
+
+    if is_zoomed then
+      tab:set_zoomed(false)
+      tab:set_zoomed(true)
+    end
+  end
+
+  return wezterm.action_callback(callback)
+end
+
+-- ON UPDATE RIGHT STATUS
+wezterm.on('update-status', function(window, pane)
+  local mode = window:active_key_table()
+
+  if mode == 'resize_mode' then
+    window:set_right_status(
+      wezterm.format {
+        { Foreground = { Color = '#1e1e2e' } },
+        { Background = { Color = '#f9e2af' } },
+        { Text = '  RESIZE  ' },
+      }
+    )
+  elseif mode == 'pane_mode' then
+    window:set_right_status(
+      wezterm.format {
+        { Foreground = { Color = '#1e1e2e' } },
+        { Background = { Color = '#89b4fa' } },
+        { Text = '  PANE  ' },
+      }
+    )
+  else
+    window:set_right_status('')
+  end
+end)
+
+config.initial_cols = 120
+config.initial_rows = 28
+
+config.font_size = 10
+
+config.default_prog = { 'nu' }
+-- config.enable_shell_integration = true
+
+config.adjust_window_size_when_changing_font_size = false
+
+-- color_scheme = 'termnial.sexy'
+config.color_scheme = 'Catppuccin Mocha'
+config.enable_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = false
+config.show_tabs_in_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+config.font_size = 12.0
+config.font = wezterm.font('FiraCode Nerd Font')
+config.status_update_interval = 100
+
+config.window_decorations = 'TITLE'
+config.window_background_opacity = 1.00
+
+local MOD_KEY = "ALT"
+
+-- BELL
+config.audible_bell = "Disabled"
+config.visual_bell = {
+  fade_in_duration_ms = 75,
+  fade_out_duration_ms = 75,
+  target = 'CursorColor',
+}
+
+config.animation_fps = 24
+config.cursor_blink_ease_in = 'Linear'
+config.cursor_blink_ease_out = 'Linear'
+config.window_background_opacity = 1.0
+config.macos_window_background_blur = 0
+
+config.hide_tab_bar_if_only_one_tab = true
+config.use_resize_increments = false
+config.use_fancy_tab_bar = true
+config.tab_bar_at_bottom = true
+
+config.tab_max_width = 200
+config.window_padding = {
+  left = '10px',
+  right = '10px',
+  top = '10px',
+  bottom = 0,
+}
+config.inactive_pane_hsb = {
+  hue = 1.0,
+  saturation = 1.0,
+  brightness = 0.1,
+}
+config.foreground_text_hsb = {
+  hue = 1.0,
+  saturation = 1.0,
+  brightness = 1.0,
+}
+local background_color = '#000000' -- same background as neovim
+--local background_color = '#080c10' -- same background as neovim
+local active_fg = "#9f7e20";
+local active_bg = background_color;
+local hover_fg = "#93a1a1";
+local hover_bg = background_color;
+local inactive_fg = "#363a41";
+local inactive_bg = background_color;
+
+local MOD_KEY = "ALT"
+
+config.colors = {
+  background = background_color, -- same background as neovim
+
+  tab_bar = {
+    background = background_color, -- dark retro teal background
+
+    active_tab = {
+      bg_color = active_bg,
+      fg_color = active_fg,
+      intensity = "Bold",
+    },
+
+    inactive_tab = {
+      bg_color = inactive_bg,
+      fg_color = inactive_fg,
+    },
+
+    inactive_tab_hover = {
+      bg_color = hover_bg,
+      fg_color = hover_fg,
+    },
+
+    new_tab = {
+      bg_color = inactive_bg,
+      fg_color = inactive_fg,
+    },
+
+    new_tab_hover = {
+      bg_color = hover_bg,
+      fg_color = hover_fg,
+      italic = true,
+    },
+  },
+
+  visual_bell = '#222020',
+}
+
+
+config.keys = {
+	{
+		key = 'q',
+		mods = MOD_KEY,
+		action = wezterm.action.ToggleFullScreen,
+	},
+  {
+    key = 'w',
+    mods = MOD_KEY,
+    action = act.ActivateKeyTable {
+      name = 'pane_mode',
+      one_shot = true,
+      prevent_fallback = true,
+    },
+  },
+  {
+    key = 'r',
+    mods = MOD_KEY,
+    action = act.ActivateKeyTable {
+      name = 'resize_mode',
+      one_shot = false,
+      prevent_fallback = true,
+    },
+  },
+  { mods = MOD_KEY, key = 'j', action = ActivateThing('next') },
+  { mods = MOD_KEY, key = 'k', action = ActivateThing('prev') },
+  { mods = MOD_KEY, key = 'l', action = act.ActivateTabRelative(1) },
+  { mods = MOD_KEY, key = 'h', action = act.ActivateTabRelative(-1) },
+  { mods = MOD_KEY, key = 'f', action = act.TogglePaneZoomState },
+  { mods = MOD_KEY, key = 'x', action = act.ShowTabNavigator },
+  { mods = MOD_KEY.."|CTRL", key = 'j', action = act.MoveTabRelative(1) },
+  { mods = MOD_KEY.."|CTRL", key = 'k', action = act.MoveTabRelative(-1) },
+}
+
+config.mouse_bindings = {
+	  -- Ctrl-click will open the link under the mouse cursor
+  {
+    event = { Up = { streak = 1, button = 'Left' } },
+    mods = 'CTRL',
+    action = wezterm.action.OpenLinkAtMouseCursor,
+  },
+}
+
+config.key_tables = {
+
+  pane_mode = {
+    -- Cancel the mode by pressing escape or enter
+    { key = 'Escape', action = 'PopKeyTable' },
+    { key = 'Enter', action = 'PopKeyTable' },
+
+    -- change panes
+    { key = 'h', action = act.ActivatePaneDirection 'Left' },
+    { key = 'l', action = act.ActivatePaneDirection 'Right' },
+    { key = 'k', action = act.ActivatePaneDirection 'Up' },
+    { key = 'j', action = act.ActivatePaneDirection 'Down' },
+    { key = 'LeftArrow', action = act.ActivatePaneDirection 'Left' },
+    { key = 'RightArrow', action = act.ActivatePaneDirection 'Right' },
+    { key = 'UpArrow', action = act.ActivatePaneDirection 'Up' },
+    { key = 'DownArrow', action = act.ActivatePaneDirection 'Down' },
+
+    -- resize mode
+    {
+      key = 'r',
+      action = act.ActivateKeyTable {
+        name = 'resize_mode',
+        one_shot = false,
+        prevent_fallback = true,
+      },
+    },
+
+    -- toggle fullscreen
+    { key = 'f', action = act.TogglePaneZoomState },
+
+    -- font size
+    { key = '+', action = act.IncreaseFontSize },
+    { key = '-', action = act.DecreaseFontSize },
+
+    -- tabs
+    { key = 't', action = act.SpawnTab 'CurrentPaneDomain' },
+    { key = 'n', action = act.ActivateTabRelative(1) },
+    { key = 'p', action = act.ActivateTabRelative(-1) },
+
+    -- splits
+    { key = 's', action = act.SplitVertical{ domain =  'CurrentPaneDomain' } },
+    { key = 'v', action = act.SplitHorizontal{ domain =  'CurrentPaneDomain' } },
+
+    -- close pane
+    { key = 'x', action = act.CloseCurrentPane{ confirm =  true } },
+
+  },
+
+
+  resize_mode = {
+    -- Cancel the mode by pressing escape or enter
+    { key = 'Escape', action = 'PopKeyTable' },
+    { key = 'Enter', action = 'PopKeyTable' },
+
+    -- resize pane
+    { key = 'h', action = act.AdjustPaneSize { 'Left', 1 } },
+    { key = 'l', action = act.AdjustPaneSize { 'Right', 1 } },
+    { key = 'k', action = act.AdjustPaneSize { 'Up', 1 } },
+    { key = 'j', action = act.AdjustPaneSize { 'Down', 1 } },
+    { key = 'LeftArrow', action = act.AdjustPaneSize { 'Left', 1 } },
+    { key = 'RightArrow', action = act.AdjustPaneSize { 'Right', 1 } },
+    { key = 'UpArrow', action = act.AdjustPaneSize { 'Up', 1 } },
+    { key = 'DownArrow', action = act.AdjustPaneSize { 'Down', 1 } },
+
+    -- move pane
+    { key = 'h', mods = 'CTRL', action = act.RotatePanes 'CounterClockwise' },
+    { key = 'l', mods = 'CTRL', action = act.RotatePanes 'Clockwise' },
+  },
+
+}
+
+config.enable_wayland = true
+
+return config
